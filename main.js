0146e2260719566257870a9b45859aced7e9e5f2
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-04-05 16:33:41 +0400

    structure,style,logic changess developing

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const startBtn = document.querySelector('.startBtn');
 const logo = document.querySelector('.img');
 const h2 = document.querySelector('.h2');
 const qst1 = document.querySelector('.qst1');
-
+const right1 = document.querySelector('.answer.right1')
+const fallse1 = document.querySelector('.false1')
+const fallse2 = document.querySelector('.false2')
+const fallse3 = document.querySelector('.false3')
+const nextBtn = document.querySelector('.next');
 
 
 let counter = 0;
@@ -13,5 +17,12 @@ function func1() {
    logo.style.display = 'none';
    startBtn.style.display = 'none';
    qst1.style.display = 'grid';
- 
+   nextBtn.style.display = 'grid';
+}
+
+function func2() {
+   right1.style.backgroundColor = 'green';
+   fallse1.style.backgroundColor = 'red';
+   fallse2.style.backgroundColor = 'red';
+   fallse3.style.backgroundColor = 'red';
 }
