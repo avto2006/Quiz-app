6b49bd59a16a894d2c7c34f7dd4ef1dd40d05be9
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-04-05 16:39:39 +0400

    structure,style,logic changess developing

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ const startBtn = document.querySelector('.startBtn');
 const logo = document.querySelector('.img');
 const h2 = document.querySelector('.h2');
 const qst1 = document.querySelector('.qst1');
+const qst2 = document.querySelector('.qst2');
+const qst3 = document.querySelector('.qst3');
+const qst4 = document.querySelector('.qst4');
+const qst5 = document.querySelector('.qst5');
+const qst6 = document.querySelector('.qst6');
+const qst7 = document.querySelector('.qst7');
+const qst8 = document.querySelector('.qst8');
+const qst9 = document.querySelector('.qst9');
+const qst10 = document.querySelector('.qst10');
 const right1 = document.querySelector('.answer.right1')
 const fallse1 = document.querySelector('.false1')
 const fallse2 = document.querySelector('.false2')
@@ -22,6 +31,15 @@ function func1() {
 
 function func2() {
    right1.style.backgroundColor = 'green';
+   fallse1.style.backgroundColor = 'red';
+   fallse2.style.backgroundColor = 'red';
+   fallse3.style.backgroundColor = 'red';
+}
+
+function func3() {
+    qst1.style.display = 'none';
+    qst2.style.display = 'grid';
+    right1.style.backgroundColor = 'green';
    fallse1.style.backgroundColor = 'red';
    fallse2.style.backgroundColor = 'red';
    fallse3.style.backgroundColor = 'red';
