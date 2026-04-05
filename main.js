e9f63f17e252e102ace614c5989ce0bae064ec59
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-04-05 15:56:04 +0400

    first changess

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-console.log([] + []);
-console.log([] + {});
-console.log({} + []);
+const startBtn = document.querySelector('.startBtn');
+const logo = document.querySelector('.img');
+const h2 = document.querySelector('.h2');
+
+
+function func1() {
+   h2.style.display = 'none'; 
+   logo.style.display = 'none';
+   startBtn.style.display = 'none';
+}
